8a9f45db2651ee44f6c2543f99d0c1088fb32ee1
Bryan Mangapit <mangapit.bryan@gmail.com>
2024-10-07 08:52:20 +0800

    added templates_folder & static folder

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
 
 
 @app.route("/")
@@ -10,4 +10,3 @@ def index():
 if __name__ == '__main__':
 	app.run(debug=True)
 
-	
