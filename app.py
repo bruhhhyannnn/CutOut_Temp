ecbc56fc072b3cf3f3e1f25b7e54d0fd43c09bb9
Bryan Mangapit <mangapit.bryan@gmail.com>
2024-10-07 09:11:57 +0800

    added header & footer for pages, rendered templates for base.html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
 
 
 @app.route("/")
 def index():
-	return "Hello World, from flask!"
+	return render_template('/base.html')
 
 
 if __name__ == '__main__':
